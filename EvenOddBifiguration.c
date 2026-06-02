2c658d255c55c0fc18108040308e17e7a9e83ea2
Siya Giri <siyagiri162@gmail.com>
2026-06-03 00:46:32 +0530

    Add EvenOddBifiguration program

---
+++ EvenOddBifiguration.c
@@ -0,0 +1,43 @@
+
+// Name: Namrata Ankush Surwase
+// Branch: EXTC(R)
+// PRN:2503033111372L003
+// Batch:A
+
+#include <stdio.h>
+
+int main() {
+    int a[10], even[10], odd[10];
+    int e = 0, o = 0;
+
+    // Input 10 numbers
+    for (int i = 0; i < 10; i++) {
+        printf("Enter value at index %d: ", i);
+        scanf("%d", &a[i]);
+    }
+
+    // Separate even and odd numbers
+    for (int i = 0; i < 10; i++) {
+        if (a[i] % 2 == 0) {
+            even[e] = a[i];
+            e++;
+        } else {
+            odd[o] = a[i];
+            o++;
+        }
+    }
+
+    // Display even numbers
+    printf("\nEven numbers:\n");
+    for (int i = 0; i < e; i++) {
+        printf("%d\t", even[i]);
+    }
+
+    // Display odd numbers
+    printf("\n\nOdd numbers:\n");
+    for (int i = 0; i < o; i++) {
+        printf("%d\t", odd[i]);
+    }
+
+    return 0;
+}
